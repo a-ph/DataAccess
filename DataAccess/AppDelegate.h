38bda63938c924bd6574ef788cfcf16dad20ecfa
eaph sing <xing.yf@yougou.com>
2017-11-23 10:59:04 +0800

    Initial Commit

---
+++ DataAccess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataAccess
+//
+//  Created by Eaph Sing on 2017/11/23.
+//  Copyright © 2017年 Ray Wenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataAccess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataAccess
+//
+//  Created by Eaph Sing on 2017/11/23.
+//  Copyright © 2017年 Ray Wenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
